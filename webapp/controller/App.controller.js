0d2189a211cf2867ceacf1d57d6f855bb9b0c872
vijay Amrit <vijay.amrit@protonmail.com>
2020-12-06 17:12:08 +0000

    Commit-3

--- webapp/controller/App.controller.js
+++ webapp/controller/App.controller.js
@@ -1,8 +1,9 @@
 sap.ui.define([
     'sap/ui/core/mvc/Controller',
     'sap/m/MessageToast',
-    'sap/ui/model/json/JSONModel'
-], function(Controller, MessageToast, JSONModel) {
+    'sap/ui/model/json/JSONModel',
+    'sap/ui/model/resource/ResourceModel'
+], function(Controller, MessageToast, JSONModel, ResourceModel) {
     'use strict';
     return Controller.extend("sap.ui.demo.FirstProj.App",{
         onInit: function(){
@@ -15,11 +16,24 @@ sap.ui.define([
             var oModel= new JSONModel(oData);
             // debugger;
             this.getView().setModel(oModel, 'mainModel');
+            // set i18n Model on view
+            var i18nModel = new ResourceModel({
+                bundleName: 'sap.ui.demo.FirstProj.i18n.i18n.properties',
+                supportedLocales: [''],
+                fallBackLocale:''
+            });
+            this.getView().setModel(i18nModel, 'i18n');
+
         },
         onShowHello:function(){
-            var inputString = this.getView().byId('InputNoEdit').getValue();
+            debugger;
+            
+            var oBundle = this.getView().getModel("i18n").getResourceBundle(); 
+            var sRecipient = this.getView().getModel("mainModel").getProperty("/recipient/name");
+            // var inputString = this.getView().byId('InputNoEdit').getValue();
+            var sMsg = oBundle.getText("helloMsg", [sRecipient]);
             // debugger;
-            MessageToast.show("Hello "+inputString+"!");
+            MessageToast.show(sMsg+"!");
         }
     });
 });
